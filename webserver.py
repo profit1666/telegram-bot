b51bde6b377bae2e80dc14edf891da61877c7455
profit1666 <profit1666@gmail.com>
2025-07-01 08:36:26 +0300

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +8,5 @@ def home():
     return "I'm alive!"
 
 def keep_alive():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)

7c9b09074308b1f36e243fb71fd7ea229d15306f
profit1666 <profit1666@gmail.com>
2025-07-01 08:15:50 +0300

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,15 +1,9 @@
 from flask import Flask
-from threading import Thread
-
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "I'm alive!"
 
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    app.run(host='0.0.0.0', port=8080)
